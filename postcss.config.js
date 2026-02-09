64ca0e6f800ba046b31167fd3a37217dcb5ff418
mymyanmarland <mymyanmarland4@gmail.com>
2026-02-09 18:28:50 +0630

    Deploy: Updated configs and final build

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+  },
 }
